253306de4594bcf809c0143a0b9a14160c8e1f4d
oledenhertog <ole.denhertog@student.hu.nl>
2023-05-03 18:23:08 +0200

    choose which existing problem to calculate route for

--- GUI.py
+++ GUI.py
@@ -1,19 +1,22 @@
 from PyQt5.QtWidgets import \
     QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QComboBox
 from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import QByteArray
 import sys
+from main import *
 
-class Example(QWidget):
+class TspGui(QWidget):
     def __init__(self):
         super().__init__()
         self.current_algorithm = 'Nearest neighbour'
-        self.initUI()
+        self.problems = ['att48', 'a280', 'bays29', 'berlin52', 'ch130', 'ch150']
 
-    def initUI(self):
         self.setWindowTitle('Travelling Salesman Problem')
+
+        run(f'TestProblems/att48.tsp')
         # Create label widget to display the GIF
         self.movie_label = QLabel(self)
-        self.movie = QMovie("animation.gif")
+        self.movie = QMovie("animation.gif", QByteArray(), self)
         self.movie_label.setMovie(self.movie)
         self.movie.start()
 
@@ -31,34 +34,52 @@ class Example(QWidget):
 
         self.combo = QComboBox(self)
         self.combo.addItem('Nearest neighbour')
-        self.combo.addItem('Algorithm 2')
-        self.combo.addItem('Algorithm 3')
         vbox1.addWidget(self.combo)
 
         self.button1 = QPushButton('submit', self)
         vbox1.addWidget(self.button1)
         self.button1.clicked.connect(self.button1_call)
 
-        hbox2 = QHBoxLayout()
-        self.x_label = QLabel()
-        self.x_label.setText('X = ')
-        hbox2.addWidget(self.x_label)
-        self.x_input = QLineEdit(self)
-        hbox2.addWidget(self.x_input)
+        self.label3 = QLabel()
+        self.label3.setText('Select an existing TSP problem:')
+        vbox1.addWidget(self.label3)
 
-        hbox3 = QHBoxLayout()
-        self.y_label = QLabel()
-        self.y_label.setText('Y = ')
-        hbox3.addWidget(self.y_label)
-        self.y_input = QLineEdit(self)
-        hbox3.addWidget(self.y_input)
+        self.combo1 = QComboBox(self)
+        for p_name in self.problems:
+            self.combo1.addItem(p_name)
+        vbox1.addWidget(self.combo1)
 
-        vbox1.addLayout(hbox2)
-        vbox1.addLayout(hbox3)
+        self.button3 = QPushButton('Recalculate route', self)
+        vbox1.addWidget(self.button3)
+        self.button3.clicked.connect(self.calculateRouteCall)
 
-        self.button2 = QPushButton('submit', self)
-        vbox1.addWidget(self.button2)
-        self.button2.clicked.connect(self.button1_call)
+        # hbox2 = QHBoxLayout()
+        # self.x_label = QLabel()
+        # self.x_label.setText('Name = ')
+        # hbox2.addWidget(self.x_label)
+        # self.x_input = QLineEdit(self)
+        # hbox2.addWidget(self.x_input)
+        # vbox1.addLayout(hbox2)
+        #
+        # hbox3 = QHBoxLayout()
+        # self.y_label = QLabel()
+        # self.y_label.setText('X = ')
+        # hbox3.addWidget(self.y_label)
+        # self.y_input = QLineEdit(self)
+        # hbox3.addWidget(self.y_input)
+        # vbox1.addLayout(hbox3)
+        #
+        # hbox4 = QHBoxLayout()
+        # self.city_label = QLabel()
+        # self.city_label.setText('Y = ')
+        # hbox4.addWidget(self.city_label)
+        # self.city_label = QLineEdit(self)
+        # hbox4.addWidget(self.city_label)
+        # vbox1.addLayout(hbox4)
+        #
+        # self.button2 = QPushButton('submit', self)
+        # vbox1.addWidget(self.button2)
+        # self.button2.clicked.connect(self.button1_call)
 
         hbox1.addLayout(vbox1)
 
@@ -71,8 +92,19 @@ class Example(QWidget):
         self.label1.setText(f'Currently using algorithm: {self.current_algorithm}')
         return
 
+    def calculateRouteCall(self):
+        print(f'{self.combo1.currentText()} chosen')
+        run(f'TestProblems/{self.combo1.currentText()}.tsp')
+        self.movie = QMovie("animation.gif", QByteArray(), self)
+        self.movie_label.setMovie(self.movie)
+        self.movie.start()
+        return
+
+
+def runGUI():
+    app = QApplication(sys.argv)
+    ex = TspGui()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+    runGUI()
