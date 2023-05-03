1f9dbd4a0ab8a2959e6bbf97dde581321aaf1319
oledenhertog <ole.denhertog@student.hu.nl>
2023-05-03 20:23:56 +0200

    add return of tour length in run()

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def animate_tour(tours: list[Tour]):
         lines = plt.plot([c.x for c in tours[frame]], [c.y for c in tours[frame]], color='blue')
         return lines
 
-    interval = int(10000 / len(tours))  # every gif/animation should take 5 seconds
+    interval = int(10000 / len(tours))  # every gif/animation should take 10 seconds
     anim = FuncAnimation(fig, update, frames=len(tours), interval=interval)
     video = anim.save('animation.gif', writer='ffmpeg')
     plt.close()
@@ -33,12 +33,12 @@ def get_tsp_problem_from_file(file_name: str) -> list[City]:
     cities = [City(str(name), tuple(pos)) for name, pos in c.items()]
     return cities
 
-def run(file_name):
+def run(file_name: str) -> float:
     cities = get_tsp_problem_from_file(file_name)
     tours = nn.run(cities)
     print(f'the algorithm took {len(tours)} steps')
     animate_tour(tours)
-    pass
+    return tours[-1].distance
 
 if __name__ == "__main__":
     run('TestProblems/berlin52.tsp')
