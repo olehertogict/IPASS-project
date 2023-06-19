b316b01d6a638f0c0906869157cf86bae134eea4
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-19 13:18:10 +0200

    del print calls

--- Algorithms/NearestNeighbour.py
+++ Algorithms/NearestNeighbour.py
@@ -6,10 +6,8 @@ from classes.Tour import Tour
 
 def nearest_neighbour_algorithm(tour: Tour, cities: list[City], tours: list[Tour]):
     if not cities:
-        print("no remaining possibilities, returning tour")
         tour.add_city(tour[0])
         tours.append(copy.copy(tour))
-        print(f"The final tour length is {tour.distance}")
         return tours
     remaining_cities = cities
     distances_from_city = {}
@@ -26,12 +24,11 @@ def nearest_neighbour_algorithm(tour: Tour, cities: list[City], tours: list[Tour
     # tours.append(copy.copy(tour))
     return nearest_neighbour_algorithm(tour, remaining_cities, tours)
 
-def run(cities) -> list[Tour]:
+def run(cities) -> Tour:
     starting_city = random.choice(cities)
     all_cities = [city for city in cities if city.name != starting_city.name]
     tour = Tour([starting_city])
     tour_steps = nearest_neighbour_algorithm(tour, all_cities, [])
-    print('algorithm done calculating')
     return tour_steps[-1]
 
 if __name__ == "__main__":
