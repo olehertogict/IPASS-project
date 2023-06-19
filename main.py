313c7f1945dde8adaee94022d1c5aea73ddfc3dc
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-19 12:13:57 +0200

    change from animation to image

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from matplotlib import animation
 from matplotlib.animation import FuncAnimation
 from classes.City import City
 from classes.Tour import Tour
+from classes.TspProblem import TspProblem
 import Algorithms.NearestNeighbour as nn
 import Algorithms.Genetic as genetic
-from pprint import pprint
+import Algorithms.TwoOpt as TwoOpt
 import copy
+import api_key
+import random
+
 
 def animate_tour(tours: list[Tour]):
     if len(tours) == 1:
@@ -22,19 +26,21 @@ def animate_tour(tours: list[Tour]):
     fig = plt.figure()
     plt.axis('off')
     plt.scatter(x_values, y_values)
+    plt.plot([c.x for c in tours[-1]], [c.y for c in tours[-1]], color='blue')
+    plt.savefig('animation.jpeg')
 
-    def update(frame):
-        lines = plt.plot([c.x for c in tours[frame]], [c.y for c in tours[frame]], color='blue')
-        return lines
-
-    interval = int(10000 / len(tours))  # every gif/animation should take 10 seconds
-    print('start animating')
-    anim = FuncAnimation(fig, update, frames=len(tours), interval=interval)
-    print('start saving')
-    writervideo = animation.FFMpegWriter()
-    anim.save(r"animation.gif", writer=writervideo)
-    print('done saving')
-    plt.close()
+    # def update(frame):
+    #     lines = plt.plot([c.x for c in tours[frame]], [c.y for c in tours[frame]], color='blue')
+    #     return lines
+    #
+    # interval = int(10000 / len(tours))  # every gif/animation should take 10 seconds
+    # print('start animating')
+    # anim = FuncAnimation(fig, update, frames=len(tours), interval=interval)
+    # print('start saving')
+    # writervideo = animation.FFMpegWriter()
+    # anim.save(r"animation.gif", writer=writervideo)
+    # print('done saving')
+    # plt.close()
 
 def get_tsp_problem_from_file(file_name: str) -> list[City]:
     problem = tsplib95.load(file_name)
@@ -44,29 +50,15 @@ def get_tsp_problem_from_file(file_name: str) -> list[City]:
     cities = [City(str(name), tuple(pos)) for name, pos in c.items()]
     return cities
 
-def run(file_name: str) -> float:
+def run(file_name: str, algorithm: str) -> float:
     cities = get_tsp_problem_from_file(file_name)
-    # tours = nn.run(cities)
-    tours = [genetic.run(cities, 1000, 200)]
-    print(f'the algorithm took {len(tours)} steps')
+    algortihms = {'Nearest Neighbour': nn, 'Evolutionary/Genetic': genetic, '2-Opt': TwoOpt}
+    tours = [algortihms[algorithm].run(cities)]
     animate_tour(tours)
     return tours[-1].distance
 
-from opencage.geocoder import OpenCageGeocode
-import api_key
 
 if __name__ == "__main__":
-    # run('TestProblems/att48.tsp')
-    geocoder = OpenCageGeocode(key=api_key.API_KEY)
-    address = 'van lieflandlaan 120'
-
-    # Geocode the address
-    result = geocoder.geocode(address)
-
-    # Get the latitude and longitude
-    lat = result[0]['geometry']['lat']
-    lng = result[0]['geometry']['lng']
-
-    print(f'Latitude: {lat}, Longitude: {lng}')
-
-    pass
+    problem = TspProblem()
+    problem.get_tsp_problem_from_file('TspProblems/att48.tsp')
+    TwoOpt.run(problem.cities)
