f0160a692eed684eb5ab5b9011e75dd1e7da7332
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-19 12:16:14 +0200

    update 2opt

--- Algorithms/TwoOpt.py
+++ Algorithms/TwoOpt.py
@@ -14,22 +14,16 @@ def two_opt_switch(tour: Tour, i: int, j: int):
 def run(cities: list[City]):
     tour = Tour(cities)
     print(tour, tour.calc_distance())
-    cur_length = tour.calc_distance2()
     n = len(tour)
     found_improvement = True
     while found_improvement:
         found_improvement = False
         for i in range(n - 1):
             for j in range(i + 1, n):
-                length_delta = (
-                    - tour[i].distance2(tour[(i + 1) % n])
-                    - tour[j].distance2(tour[(j + 1) % n])
-                    + tour[i].distance2(tour[j])
-                    + tour[(i + 1) % n].distance2(tour[(j + 1) % n])
-                )
-                if length_delta < 0:
+                sum_plus = tour[i].distance2(tour[j]) + tour[(i + 1) % n].distance2(tour[(j + 1) % n])
+                sum_neg = tour[i].distance2(tour[(i + 1) % n]) + tour[j].distance2(tour[(j + 1) % n])
+                if sum_plus - sum_neg < 0:
                     two_opt_switch(tour, i, j)
-                    cur_length += length_delta
                     found_improvement = True
     tour.add_city(tour[0])
     print(tour, tour.calc_distance())
