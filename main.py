77db34a25fcddb39a7e35e1d14f3b160bbd91eef
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-21 14:44:08 +0200

    add data export to csv file

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from classes.Tour import Tour
 import Algorithms.NearestNeighbour as NearestNeighbour
 import Algorithms.Genetic as Genetic
 import Algorithms.TwoOpt as TwoOpt
+import GUI
 import api_key
+from time import time
+import csv
 
 def create_tour_image(tour: Tour):
     all_positions = [c.get_coordinates() for c in tour.cities]
@@ -13,6 +16,8 @@ def create_tour_image(tour: Tour):
     # setup plot
     fig = plt.figure()
     plt.axis('off')
+    img = plt.imread("maps_image.jpeg")
+    plt.imshow(img, extent=[min(x_values)-10, max(x_values)+10, min(y_values)-10, max(y_values)+10])
     plt.scatter(x_values, y_values)
     plt.plot([c.x for c in tour], [c.y for c in tour], color='blue')
     plt.savefig('animation.jpeg')
@@ -25,10 +30,29 @@ def get_tsp_problem_from_file(file_name: str) -> list[City]:
     cities = [City(str(name), tuple(pos)) for name, pos in c.items()]
     return cities
 
-def run(file_name: str, algorithm: str) -> float:
-    cities = get_tsp_problem_from_file(file_name)
-    algortihms = {'Nearest Neighbour': NearestNeighbour, 'Evolutionary/Genetic': Genetic, '2-Opt': TwoOpt}
-    tour = algortihms[algorithm].run(cities)
+def calc_route(file_name: str, algorithm: str) -> float:
+    cities = get_tsp_problem_from_file(f'TestProblems/{file_name}.tsp')
+    algorithms = {'Nearest Neighbour': NearestNeighbour, 'Evolutionary/Genetic': Genetic, '2-Opt': TwoOpt}
+    t1 = time()
+    tour = algorithms[algorithm].run(cities)
+    t2 = time()
+    print(f'ran in: {t2-t1}')
+    data = {
+        'problem': file_name,
+        'algo': algorithm,
+        'time': t2 - t1,
+        'distance': round(tour.calc_distance())
+    }
+    with open('time_data.csv', 'a', newline='') as csvfile:
+        fieldnames = ['problem', 'algo', 'time', 'distance']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(data)
+
     create_tour_image(tour)
     return tour.calc_distance()
 
+def main():
+    GUI.runGUI()
+
+if __name__ == "__main__":
+    main()
