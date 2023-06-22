1c4ac1d7981983fc7b1dcc99670e4f581f3c22c7
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-22 14:33:28 +0200

    add docstrings

--- Algorithms/NearestNeighbour.py
+++ Algorithms/NearestNeighbour.py
@@ -1,32 +1,39 @@
-import copy
 import random
 from Classes.City import City
 from Classes.Tour import Tour
 
-def nearest_neighbour_algorithm(tour: Tour, cities: list[City], tours: list[Tour]):
-    if not cities:
-        tour.add_city(tour[0])
-        tours.append(copy.copy(tour))
-        return tours
+def nearest_neighbour_algorithm(cities: list[City]):
+    """
+    Calculate a route using the nearest neighbour algorithm. Start at a random city and choose the next closest
+    city until no more cities are available.
+
+    :param cities: A list of cities to calculate the 'optimal' route of
+    :return: the tour found by the algorithm
+    """
     remaining_cities = cities
-    distances_from_city = {}
-    for city in remaining_cities:
-        distances_from_city[city] = tour[-1].distance2(city)
-    # Get minimum distance from current city
-    min_distance = min(distances_from_city.values())
-    # Get city with minimum distance
-    closest_city = [key for key, value in distances_from_city.items() if value == min_distance][0]
-    tour.add_city(closest_city)
-    # Make list of remaining cities to choose from next iteration
-    remaining_cities = [city for city in remaining_cities if city != closest_city]
-    # Append a copy of this tour to the list of tours (for animation)
-    return nearest_neighbour_algorithm(tour, remaining_cities, tours)
+    current_city = random.choice(cities)
+    tour = Tour([current_city])
+    while remaining_cities:
+        # current city is the last city in the tour
+        current_city = tour[-1]
+        distances_from_city = {}
+        for city in remaining_cities:
+            distances_from_city[city] = current_city.distance2(city)
+        closest_city = min(distances_from_city, key=distances_from_city.get)
+        tour.add_city(closest_city)
+        remaining_cities.remove(closest_city)
+
+    return tour
 
 def run(cities: list[City]) -> Tour:
-    starting_city = random.choice(cities)
-    all_cities = [city for city in cities if city.name != starting_city.name]
-    tour = Tour([starting_city])
-    tour_steps = nearest_neighbour_algorithm(tour, all_cities, [])
-    return tour_steps[-1]
+    """
+    run the nearest neighbour algorithm given cities
+
+    :param cities: a list of cities to calculate a route from
+    :return: the route found by the algorithm
+    """
+    tour = nearest_neighbour_algorithm(cities)
+    tour.add_city(tour[0])
+    return tour
 
 
