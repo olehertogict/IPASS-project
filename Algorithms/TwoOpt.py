4f04b96f07220e41e959acbf5815f2e764f4376c
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-22 14:43:58 +0200

    add docstrings

--- Algorithms/TwoOpt.py
+++ Algorithms/TwoOpt.py
@@ -4,6 +4,7 @@ from Classes.Tour import Tour
 def two_opt_switch(tour: Tour, i: int, j: int) -> None:
     """
     reverse order of the cities between the indices
+
     :param tour: the tour where the cities will be switched
     :param i: starting index
     :param j: ending index
@@ -14,16 +15,26 @@ def two_opt_switch(tour: Tour, i: int, j: int) -> None:
     tour.cities[i+1:j+1] = reversed_tour
 
 def run(cities: list[City]) -> Tour:
+    """
+    Calculate the optimal route using the 2-opt algorithm.
+
+    :param cities: a list of cities for which to calculate a route for
+    :return: The best route found by the 2-opt algorithm
+    """
     tour = Tour(cities)
     n = len(tour)
     found_improvement = True
     while found_improvement:
         found_improvement = False
+        # iterate over pairs of cities
         for i in range(n - 1):
             for j in range(i + 1, n):
-                sum_plus = tour[i].distance2(tour[j]) + tour[(i + 1) % n].distance2(tour[(j + 1) % n])
-                sum_neg = tour[i].distance2(tour[(i + 1) % n]) + tour[j].distance2(tour[(j + 1) % n])
-                if sum_plus - sum_neg < 0:
+                # current distance of i to j + (i+1)%n to (j+1)%n
+                cur_dist = tour[i].distance2(tour[j]) + tour[(i + 1) % n].distance2(tour[(j + 1) % n])
+                # potential improvement i to (i+1)%n + j to (j+1)%n (the switch of cities)
+                potential_improvement = tour[i].distance2(tour[(i + 1) % n]) + tour[j].distance2(tour[(j + 1) % n])
+                # if there is an improvement found to the switch
+                if cur_dist - potential_improvement < 0:
                     two_opt_switch(tour, i, j)
                     found_improvement = True
 
