7a563b32ec2b34b913b374673794db47db7a6329
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-22 14:14:14 +0200

    add docstrings

--- Algorithms/Genetic.py
+++ Algorithms/Genetic.py
@@ -1,16 +1,30 @@
 from Classes.Tour import Tour
+from Classes.City import City
 import random
 import copy
 import Algorithms.NearestNeighbour as NearestNeighbour
 
-def select_parents(population):
+def select_parents(population: list[Tour]) -> list[Tour]:
+    """
+    From a list of Tours select the best 50% and return those parents
+
+    :param population: The population from which to select the best routes
+    :return: A list of parents (best routes)
+    """
     sorted_population = sorted(population, key=lambda ind: ind.distance)
     # 50% of population will be parents
     num_parents = len(population) // 2
     parents = sorted_population[:num_parents]
     return parents
 
-def mutate(individual, mutation_chance):
+def mutate(individual: Tour, mutation_chance: float) -> Tour:
+    """
+    Mutate a route (individual) which a certain chance of mutation
+
+    :param individual: The tour to mutate
+    :param mutation_chance: The chance the individual has to be mutated
+    :return: The individual that maybe is mutated
+    """
     i, j = random.sample(range(len(individual)), 2)
     # use mutation chance
     if random.random() < mutation_chance:
@@ -18,7 +32,14 @@ def mutate(individual, mutation_chance):
         individual.swap_cities(i, j)
     return individual
 
-def crossover(parent1, parent2):
+def crossover(parent1: Tour, parent2: Tour) -> Tour:
+    """
+    Return a child from 2 parents, using crossover rule
+
+    :param parent1: Parent 1 to create a child with
+    :param parent2: Parent 2 to create a child with
+    :return: The child generated from the 2 parents
+    """
     # exlude last city, which is same as first city, from tour
     parent1 = parent1[:len(parent1) - 1]
     parent2 = parent2[:len(parent2) - 1]
@@ -39,7 +60,14 @@ def crossover(parent1, parent2):
     child = Tour(child)
     return child
 
-def create_children(parents, number_of_children):
+def create_children(parents: list[Tour], number_of_children: int) -> list[Tour]:
+    """
+    Create n (number_of_children) children from the given parents
+
+    :param parents: parents to create children from
+    :param number_of_children: amount of children to be created
+    :return: the generated children
+    """
     children = []
     for i in range(number_of_children):
         parent1 = random.choice(parents)
@@ -52,11 +80,26 @@ def create_children(parents, number_of_children):
 
     return children
 
-def selection(population, max_population):
+def selection(population: list[Tour], max_population: int) -> list[Tour]:
+    """
+    Select the n (max_population) fittest individuals from given population
+
+    :param population: The population to select from
+    :param max_population: The number of individuals to return
+    :return: the n (max_population) fittest individuals
+    """
     sorted_population = sorted(population, key=lambda ind: ind.distance)
     return sorted_population[:max_population]
 
-def run(cities, POPULATION_SIZE=100, MAX_GENERATION=200):
+def run(cities: list[City], POPULATION_SIZE: int = 100, MAX_GENERATION: int = 200) -> Tour:
+    """
+    Run the Genetic algorithm with given cities
+
+    :param cities: a list of cities to calculate 'optimal' route from
+    :param POPULATION_SIZE: the amount of individuals that survive each generation
+    :param MAX_GENERATION: number of generations to run the algorithm with
+    :return: the best tour found
+    """
     # generate initial random population
     population = []
     for i in range(POPULATION_SIZE):
