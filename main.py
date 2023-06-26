c5752fbab817f1e7a8eebfe8913f3777dbc1e214
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-26 09:54:53 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import tsplib95
 import matplotlib.pyplot as plt
-from classes.City import City
-from classes.Tour import Tour
+from Classes.City import City
+from Classes.Tour import Tour
 import Algorithms.NearestNeighbour as NearestNeighbour
 import Algorithms.Genetic as Genetic
 import Algorithms.TwoOpt as TwoOpt
-import GUI
-import api_key
+from Application import GUI
 from time import time
 import csv
 
-def create_tour_image(tour: Tour):
+def create_tour_image(tour: Tour) -> None:
     all_positions = [c.get_coordinates() for c in tour.cities]
     x_values, y_values = [i[0] for i in all_positions], [i[1] for i in all_positions]
-    # setup plot
     fig = plt.figure(figsize=(10, 8))
     plt.axis('off')
-    img = plt.imread("images/maps_image.jpeg")
+    img = plt.imread("Images/maps_image.jpeg")
     plt.imshow(img, extent=[min(x_values)-10, max(x_values)+10, min(y_values)-10, max(y_values)+10])
-    plt.scatter(x_values, y_values)
+    plt.scatter(x_values, y_values, color='red')
     plt.plot([c.x for c in tour], [c.y for c in tour], color='blue')
-    plt.savefig('images/animation.jpeg', bbox_inches='tight')
+    plt.xlim(min(x_values) - 10, max(x_values) + 10)
+    plt.ylim(min(y_values) - 10, max(y_values) + 10)
+    plt.savefig('Images/animation.jpeg', bbox_inches='tight')
 
 def get_tsp_problem_from_file(file_name: str) -> list[City]:
     problem = tsplib95.load(file_name)
@@ -30,26 +30,28 @@ def get_tsp_problem_from_file(file_name: str) -> list[City]:
     cities = [City(str(name), tuple(pos)) for name, pos in c.items()]
     return cities
 
-def calc_route(file_name: str, algorithm: str) -> float:
-    cities = get_tsp_problem_from_file(f'TestProblems/{file_name}.tsp')
+def calc_route(file_name: str, algorithm: str, city_list: list[City] = None) -> dict:
+    if not city_list:
+        cities = get_tsp_problem_from_file(f'TestProblems/{file_name}.tsp')
+    else:
+        cities = city_list
     algorithms = {'Nearest Neighbour': NearestNeighbour, 'Evolutionary/Genetic': Genetic, '2-Opt': TwoOpt}
     t1 = time()
     tour = algorithms[algorithm].run(cities)
     t2 = time()
-    print(f'ran in: {t2-t1}')
     data = {
         'problem': file_name,
         'algo': algorithm,
         'time': t2 - t1,
         'distance': round(tour.calc_distance())
     }
-    with open('time_data.csv', 'a', newline='') as csvfile:
+    with open('Data/time_data.csv', 'a', newline='') as csvfile:
         fieldnames = ['problem', 'algo', 'time', 'distance']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(data)
 
     create_tour_image(tour)
-    return tour.calc_distance()
+    return data
 
 def main():
     GUI.runGUI()
