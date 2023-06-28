17bab3cb46218efe0be0bb509021c9eb30ef4605
oledenhertog <ole.denhertog@student.hu.nl>
2023-06-28 15:45:24 +0200

    update main.py to use utils.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tsplib95
 import matplotlib.pyplot as plt
 from Classes.City import City
 from Classes.Tour import Tour
+from Utils import utils
 import Algorithms.NearestNeighbour as NearestNeighbour
 import Algorithms.Genetic as Genetic
 import Algorithms.TwoOpt as TwoOpt
@@ -9,30 +10,10 @@ from Application import GUI
 from time import time
 import csv
 
-def create_tour_image(tour: Tour) -> None:
-    all_positions = [c.get_coordinates() for c in tour.cities]
-    x_values, y_values = [i[0] for i in all_positions], [i[1] for i in all_positions]
-    fig = plt.figure(figsize=(10, 8))
-    plt.axis('off')
-    img = plt.imread("Images/maps_image.jpeg")
-    plt.imshow(img, extent=[min(x_values)-10, max(x_values)+10, min(y_values)-10, max(y_values)+10])
-    plt.scatter(x_values, y_values, color='red')
-    plt.plot([c.x for c in tour], [c.y for c in tour], color='blue')
-    plt.xlim(min(x_values) - 10, max(x_values) + 10)
-    plt.ylim(min(y_values) - 10, max(y_values) + 10)
-    plt.savefig('Images/animation.jpeg', bbox_inches='tight')
-
-def get_tsp_problem_from_file(file_name: str) -> list[City]:
-    problem = tsplib95.load(file_name)
-    problem_dict = problem.as_name_dict()
-    # File has to have a 'NODE_COORD_SECTION'
-    c = problem_dict.get('node_coords')
-    cities = [City(str(name), tuple(pos)) for name, pos in c.items()]
-    return cities
 
 def calc_route(file_name: str, algorithm: str, city_list: list[City] = None) -> dict:
     if not city_list:
-        cities = get_tsp_problem_from_file(f'TestProblems/{file_name}.tsp')
+        cities = utils.get_tsp_problem_from_file(f'TestProblems/{file_name}.tsp')
     else:
         cities = city_list
     algorithms = {'Nearest Neighbour': NearestNeighbour, 'Evolutionary/Genetic': Genetic, '2-Opt': TwoOpt}
@@ -45,12 +26,12 @@ def calc_route(file_name: str, algorithm: str, city_list: list[City] = None) -> 
         'time': t2 - t1,
         'distance': round(tour.calc_distance())
     }
-    with open('Data/time_data.csv', 'a', newline='') as csvfile:
+    with open('Results/time_data.csv', 'a', newline='') as csvfile:
         fieldnames = ['problem', 'algo', 'time', 'distance']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(data)
 
-    create_tour_image(tour)
+    utils.create_tour_image(tour)
     return data
 
 def main():
